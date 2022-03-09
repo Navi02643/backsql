1bb6b154f76b3f62e71222bfbd377cc53d0a4d7e
Carlos Ivan Mercado Marin <karlos123jun@gmail.com>
2022-03-09 16:11:32 -0600

    puerba log 2

--- logs/logger.js
+++ logs/logger.js
@@ -1,19 +1,24 @@
 const { createLogger, format, transports } = require("winston");
 
+let date = new Date();
+let fecha = String(date.getDate()).padStart(2, '0') + '-' + String(date.getMonth() + 1).padStart(2, '0') + '-' + date.getFullYear();
+
 module.exports = createLogger({
-    format: format.combine(
-      format.simple(),
-      format.timestamp(),
-      format.printf((info) => `[${info.timestamp} ${info.level} ${info.message}] `)
-    ),
-    transports: [
-      new transports.File({
-        maxsize: 5120000,
-        maxFiles: 10,
-        filename: `${__dirname}/log-api.log`,
-      }),
-      new transports.Console({
-          level:'debug'
-      })
-    ],
-  });
+  format: format.combine(
+    format.simple(),
+    format.timestamp(),
+    format.printf(
+      (info) => `[${info.timestamp} ${info.level} ${info.message}] `
+    )
+  ),
+  transports: [
+    new transports.File({
+      maxsize: 5120000,
+      maxFiles: 10,
+      filename: `${__dirname}/log-api_${fecha}.log `,
+    }),
+    new transports.Console({
+      level: "debug",
+    }),
+  ],
+});
